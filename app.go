9acf122aa00b6f776b6dfd15fac6623a37a49afd
scottmotte <scott@scottmotte.com>
2014-09-13 17:05:15 -0700

    Respond in a json-spec format

--- app.go
+++ app.go
@@ -3,51 +3,105 @@ package main
 import (
 	"encoding/json"
 	"github.com/go-martini/martini"
+	"github.com/handshakejs/handshakejserrors"
 	"github.com/iron-io/iron_go/mq"
 	"github.com/joho/godotenv"
+	"github.com/martini-contrib/binding"
 	"github.com/martini-contrib/render"
 	"net/http"
 	"os"
 )
 
+const (
+	LOGIC_ERROR_CODE_UNKNOWN = "unknown"
+)
+
+var (
+	QUEUE string
+)
+
+func CrossDomain() martini.Handler {
+	return func(res http.ResponseWriter) {
+		res.Header().Add("Access-Control-Allow-Origin", "*")
+	}
+}
+
+type Document struct {
+	Url     string `form:"url" json:"url"`
+	Webhook string `form:"webhook" json:"webhook"`
+}
+
 func main() {
-	godotenv.Load()
+	loadEnvs()
 
 	m := martini.Classic()
 	m.Use(martini.Logger())
 	m.Use(render.Renderer())
 
-	m.Any("/api/v0/documents/create.json", DocumentsCreate)
+	m.Any("/api/v0/documents/create.json", binding.Bind(Document{}), DocumentsCreate)
 
 	m.Run()
 }
 
-func DocumentsCreate(req *http.Request, r render.Render) {
-	_url := req.URL.Query().Get("url")
-	webhook := req.URL.Query().Get("webhook")
+func ErrorPayload(logic_error *handshakejserrors.LogicError) map[string]interface{} {
+	error_object := map[string]interface{}{"code": logic_error.Code, "field": logic_error.Field, "message": logic_error.Message}
+	errors := []interface{}{}
+	errors = append(errors, error_object)
+	payload := map[string]interface{}{"errors": errors}
+
+	return payload
+}
+
+func DocumentsPayload(document map[string]interface{}) map[string]interface{} {
+	documents := []interface{}{}
+	documents = append(documents, document)
+	payload := map[string]interface{}{"documents": documents}
+
+	return payload
+}
+
+func DocumentsCreate(document Document, req *http.Request, r render.Render) {
+	_url := document.Url
+	webhook := document.Webhook
 
 	pages := []string{}
-	document := map[string]interface{}{"url": _url, "webhook": webhook, "status": "processing", "pages": pages}
-	payload := map[string]interface{}{"success": true, "document": document}
+	params := map[string]interface{}{"url": _url, "webhook": webhook, "status": "processing", "pages": pages}
 
-	_, err := addToQueue(document)
-	if err != nil {
-		error_object := map[string]interface{}{"message": err.Error()}
-		err_payload := map[string]interface{}{"success": false, "error": error_object}
-		r.JSON(500, err_payload)
+	_, logic_error := addToQueue(params)
+	if logic_error != nil {
+		payload := ErrorPayload(logic_error)
+		statuscode := determineStatusCodeFromLogicError(logic_error)
+		r.JSON(statuscode, payload)
 	} else {
+		payload := DocumentsPayload(params)
 		r.JSON(200, payload)
 	}
 }
 
-func addToQueue(document interface{}) (string, error) {
+func addToQueue(document interface{}) (string, *handshakejserrors.LogicError) {
 	marshaled_document, _ := json.Marshal(document)
-	queue := mq.New(os.Getenv("QUEUE"))
+	queue := mq.New(QUEUE)
 
 	_, err := queue.PushString(string(marshaled_document))
 	if err != nil {
-		return "", err
+		logic_error := &handshakejserrors.LogicError{"unknown", "", err.Error()}
+		return "", logic_error
 	}
 
 	return "", nil
+}
+
+func determineStatusCodeFromLogicError(logic_error *handshakejserrors.LogicError) int {
+	code := 400
+	if logic_error.Code == LOGIC_ERROR_CODE_UNKNOWN {
+		code = 500
+	}
+
+	return code
+}
+
+func loadEnvs() {
+	godotenv.Load()
+
+	QUEUE = os.Getenv("QUEUE")
 }
